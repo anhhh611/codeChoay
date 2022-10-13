555697c1247b964bbbe1dac8501ae887c14a266f
anhhh611 <anhhh611@gmail.com>
2022-10-13 12:30:35 +0700

    Create script2.js

---
+++ javaScript/script2.js
@@ -0,0 +1,13 @@
+//ĐỀ BÀI:Tạo 1 file script2.js cùng cấp với idnex.html (nhớ import file mới này vào trong index.html). script2.js có các đoạn code sau:
+
+//2. Thực tiện thử thao tác copy các trường từ object a sang object b (a và b có các trường tùy ý, nhưng chúng có ít nhất 2 trường trùng tên nhau). Console log ra giá trị của a và b sau đó.
+//3. cho 1 JSON như sau: const x = '{name: 'x', age: 30, gender: 'male'}', hãy chuyển đổi JSON này thành object trong JS. Sau đó thực hiện thay đổi/thêm bớt giá trị trường. Cuối cùng hãy chuyển object đã được thay đổi trở về 1 JSON
+//4. Tạo 1 array có tối thiểu 5 item, mỗi item là 1 kiểu dữ liệu khác nhau (trong đó có 1 nested array). Thực hiện các thao tác sau theo đúng thứ tự: thêm 1 item vào cuối, xóa item đầu tiên, thay đổi giá trị item thứ 3, xóa giá trị thứ 2. Console.log ra array sau đó để kiểm tra kết quả
+//Tách 1 nhánh tên là js-2 từ nhánh chính và tạo 1 PR chứa file mới này vào nhánh chính.
+//Dán link PR làm câu trả lời
+
+//1. Tạo 1 biến x có giá trị là 1 object có các trường bất kỳ, sau đó tạo 1 biến y = biến x. Thay đổi giá trị của y. Console log ra giá trị của x và y sau đó.
+const x = {
+    name: "Hải Bông",
+    age : 1,
+  };
