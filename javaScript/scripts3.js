0a2531aecf7a6a3c05d008b7b2db932f57cd3c02
anhhh611 <anhhh611@gmail.com>
2022-10-15 09:41:12 +0700

    Create scripts3

---
+++ javaScript/scripts3.js
@@ -0,0 +1,11 @@
+//Tách nhánh js-2 từ nhánh chính, tạo 1 file mới scripts3.js có nội dung chứa các code sau
+//1. Tạo 2 function tên và nội dung tùy ý theo 2 cách Function declaration và Function expression
+//2. Tạo 1 function có 3 parameters, nội dung là console log ra 3 parameters đó. Call function đó với 3 trường hợp: call với 3 arguments, 2 arguments và 0 argument
+//3. Tạo 1 function nhận vào 1 parameter là 1 object bất kỳ. Mutate object đó trong nội dung function. Console.log object đó trước và sau khi gọi function
+//4. Tạo 1 function nhận vào 2 parameters. Nội dung function là console log ra (parameter1 + parameter 2). Call function đó 4 lần.
+//Lần 1: argument 1 là 1 number, argument 2 là 1 number.
+//Lần 2: argument 1 là 1 number, argument 2 là 1 string.
+//Lần 3: argument 1 là null, argument 2 là 1 number
+//Lần 4: argument 1 là number, không truyền vào argument 2
+//* tự dự đoán kết quả sẽ log ra console trước khi gọi để đối chiếu
+//Tạo 1 PR chứa file thay đổi này từ nhánh js-2 vào nhánh chính, dán link PR làm câu trả lời
