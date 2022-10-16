146ad3775bfda98ae53e7f0e0b0d991643309787
anhhh611 <anhhh611@gmail.com>
2022-10-16 13:35:06 +0700

    Update 1 + 2

--- script4.js
+++ script4.js
@@ -1,15 +1,37 @@
-//Tách nhánh js-4 từ nhánh chính, trong nhánh này tạo 1 file mới script4.js. script4 có các nội dung sau:
+//1. Tạo 1 function log ra 1 lời chào đến user, 
+//nhận vào 1 tham số là name có giá trị default là 'User', 
+//nội dung function là log ra console "Hello " + tham số name. 
 
-//- Tạo 1 function log ra 1 lời chào đến user, nhận vào 1 tham số là name có giá trị default là 'User', nội dung function là log ra console "Hello " + tham số name. Call function này 2 lần: có truyền vào 1 cái tên và không truyền vào gì
+function function1(User) { 	
+    console.log ('Hello' + User);	
+};
 
-//- Tạo 1 function có 4 parameters, trong đó có 2 default parameters và các default parameters này sẽ dùng giá trị liên quan đến các parameters khác
+//Call function này 2 lần: có truyền vào 1 cái tên và không truyền vào gì
+function1('Hai Anh'); // HelloHai Anh
+function1(); // Helloundefined
 
-//- Tạo 4 function, đều nhận vào 2 parameters, có tên là add, sub, mul, div lần lượt có tác dụng sau: trả về kết quả tổng, hiệu, tích, thương của 2 parameters. VD add(1,2) sẽ trả về 3. mul(1,2) sẽ trả về 2. Lưu ý: dấu nhân trong js sẽ là ký tự asterisk (*), dấu chia trong js sẽ là ký tự slash (/).
+//2. Tạo 1 function có 4 parameters, trong đó có 2 default parameters 
+// các default parameters này sẽ dùng giá trị liên quan đến các parameters khác
+function function2 (a, b = a + 1, c = b + 2, d) {    
+    console.log(a, b, c, d) 			       
+};
+//function2 (1, 2, 3, 4); // 1 2 3 4
 
-//- Tạo 1 function fuelCalc có tác dụng tính giá xăng RON 95-III, nhận vào 1 tham số là số lít xăng và sẽ trả về kết quả là số lít xăng nhân với 22000. VD a(10) sẽ trả về 220000
-//Tạo 1 function waveAlpha2015FuelCalc nhận vào 1 tham số là 1 function. Function b có nội dung sau: tạo 1 biến maximum có giá trị là số lít xăng tối đa của xe Wave Alpha 2015 (tra gg) và kết quả trả về của function này sẽ là gọi function nhận vào làm tham số với đối số là biến maximum trên.
-//Gọi function waveAlpha2015FuelCalc để xem đổ đầy bình xe Wave Alpha 2015 sẽ hết bn xèng
 
-//- Tạo 1 function có nội dung chứa 3 biến được khai báo bằng let / const / var. thử truy cập 3 biến đó từ 1 function bên ngoài function trên.
+//3. Tạo 4 function, đều nhận vào 2 parameters, có tên là add, sub, mul, div
+// lần lượt có tác dụng sau: trả về kết quả tổng, hiệu, tích, thương của 2 parameters. 
+//VD add(1,2) sẽ trả về 3. mul(1,2) sẽ trả về 2. Lưu ý: dấu nhân trong js sẽ là ký tự asterisk (*), dấu chia trong js sẽ là ký tự slash (/).
 
-//- Tạo 1 PR và dán link Pr làm câu trả lời
+//4. Tạo 1 function fuelCalc có tác dụng tính giá xăng RON 95-III, nhận vào 1 tham số là số lít xăng và sẽ trả về kết quả là số lít xăng nhân với 22000. 
+//VD a(10) sẽ trả về 220000
+
+
+//5. Tạo 1 function waveAlpha2015FuelCalc nhận vào 1 tham số là 1 function. 
+//Function b có nội dung sau: tạo 1 biến maximum có giá trị là số lít xăng tối đa của xe Wave Alpha 2015 (tra gg) 
+//và kết quả trả về của function này sẽ là gọi function nhận vào làm tham số với đối số là biến maximum trên.
+
+
+//6. Gọi function waveAlpha2015FuelCalc để xem đổ đầy bình xe Wave Alpha 2015 sẽ hết bn xèng
+
+//7. Tạo 1 function có nội dung chứa 3 biến được khai báo bằng let / const / var. thử truy cập 3 biến đó từ 1 function bên ngoài function trên.
+
