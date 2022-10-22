8ea5dbf368573ec0e12eb7ba4d86ec78d7147c0d
anhhh611 <anhhh611@gmail.com>
2022-10-22 11:32:12 +0700

    Update B2 + 4  + 5 + 6

--- test2.js
+++ test2.js
@@ -45,35 +45,77 @@ const e8 = !Boolean(1) || true; //true  (false || true)
 const e9 = Boolean(0) || 0; //0  (false || 0 )
 const e10 = Number("12a") && !null; //NaN  (NaN && true)
 
-// B2. Tạo function isSameType(x, y) nhận vào 2 tham số x và y có kiểu dữ liệu bất kỳ, kiểm tra xem x và y có cùng kiểu dữ liệu hay không. Nếu có thì trả về true, ngược lai trả về false
-// VD: isSameType(1, 1) // true isSameType(1, '1') // false
+// B2. Tạo function isSameType(x, y) nhận vào 2 tham số x và y có kiểu dữ liệu bất kỳ, kiểm tra xem x và y có cùng kiểu dữ liệu hay không.
+//Nếu có thì trả về true, ngược lai trả về false
+function isSameType(x, y) {
+  if (typeof x === typeof y) {
+    return true;
+  } else {
+    return false;
+  }
+}
+isSameType(1, 1); // true
+isSameType(1, "1"); // false
 
-// B3. Tạo function isAuthenticaed(x, y) nhận vào 2 tham số x và y. x là 1 giá trị boolean tượng trưng cho việc user nay
+// B3. Tạo function isAuthenticaed(x, y) nhận vào 2 tham số x và y.
+//x là 1 giá trị boolean tượng trưng cho việc user nay
 // VD: isSameType(1, 1) // true isSameType(1, '1') // false
+// function isAuthenticaed(x1, y1){
+//     if () {
+//       return
+//     } else{
+//         return
+//     }
+// }
+// isAuthenticaed(1, 1) // true
+// isAuthenticaed(1, '1') // false
 
-// B4. Tạo function isNewFemaleUser(x) nhận vào tham số x mô tả 1 user có dạng như sau:
+// B4. Tạo function  nhận vào tham số x mô tả 1 user có dạng như sau:
 // {
 // id: sẽ là 1 number bất kỳ
 // gender: sẽ là 'male' hoặc 'female',
 // joinDate: sẽ là 1 string có dạng 'dd/mm/yyyy'
 // }
-// Kiểm tra xem user x này có phải là 1 user nữ và mới đăng ký trong năm nay (2022) hay không. Nếu đúng thì trả về id của user đó, nếu sai thì trả về false
-// VD:
-// isNewFemaleUser({id: 1, gender: 'male', joinDate:'1/1/2022' }) // false
-// isNewFemaleUser({id: 2, gender: 'female', joinDate:'1/1/2022' }) // 2
-// isNewFemaleUser({id: 3, gender: 'female', joinDate:'1/1/2021' }) // false
+// Kiểm tra xem user x này có phải là 1 user nữ và mới đăng ký trong năm nay (2022) hay không.
+//Nếu đúng thì trả về id của user đó, nếu sai thì trả về false
+let x3 = {
+  id: 1,
+  gender: "female",
+  joinDate: "22/10/2022",
+};
+function isNewFemaleUser(x3) {
+  if (x3.gender === "female" && x3.joinDate.includes("2022")) {
+    return x3.id;
+  } else {
+    return false;
+  }
+}
+isNewFemaleUser({ id: 1, gender: "male", joinDate: "1/1/2022" }); // false
+isNewFemaleUser({ id: 2, gender: "female", joinDate: "1/1/2022" }); // 2
+isNewFemaleUser({ id: 3, gender: "female", joinDate: "1/1/2021" }); // false
 
 // B5. Tạo function isAuthorized(x) nhận vào tham số x mô tả 1 user có dạng như sau:
 // {
 // role: sẽ là 'admin' hoặc 'normal',
 // rank: sẽ là 1 number số nguyên có giá trị từ 0 đến 3 (0, 1, 2, 3)
 // }
-// 1 user sẽ có quyền truy cập nếu như user đó có role là 'admin' hoặc có rank lớn hơn 1. Kiểm tra xem user x có quyền truy cập hay không, nếu có thì trả về true, không thì trả về false
-// VD:
-// isAuthorized({role: 'admin', rank: 1 }) // true
-// isAuthorized({role: 'admin', rank: 2 }) // true
-// isAuthorized({role: 'normal', rank: 0 }) // false
-// isAuthorized({role: 'normal', rank: 3 }) // true
+// 1 user sẽ có quyền truy cập nếu như user đó có role là 'admin' hoặc có rank lớn hơn 1.
+//Kiểm tra xem user x có quyền truy cập hay không, nếu có thì trả về true, không thì trả về false
+let x5 = {
+  role: "admin",
+  rank: [0, 1, 2, 3],
+};
+function isAuthorized(x5) {
+  if (x5.role === "admin" || x5.rank > 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+isAuthorized({ role: "admin", rank: 1 }); // true
+isAuthorized({ role: "admin", rank: 2 }); // true
+isAuthorized({ role: "normal", rank: 0 }); // false
+isAuthorized({ role: "normal", rank: 3 }); // true
 
 // B6. Tạo function isPayable(x, y) nhận vào 2 tham số x, y
 // - tham sô x mô tả tình trạng balance (số dư tài khoản) của user, có dạng như sau:
@@ -83,12 +125,24 @@ const e10 = Number("12a") && !null; //NaN  (NaN && true)
 // }
 // - tham sô y mô tả giá tiền của 1 món hàng, sẽ là 1 số nguyên dương bất kỳ
 
-// Nếu như user không đủ tiền trả cho món hàng (balance ít hơn giá tiền) thì trả về false, còn không trả về true. Tuy nhiên nếu như user đó là  (isVIP là true) thì dù không đủ tiền vẫn cho phép mua, trả về true
-// VD:
-// isPayable({balance: 1000000, isVIP: false }, 1000) // true
-// isPayable({balance: 1000, isVIP: false }, 1000000) // false
-// isPayable({balance: 1000000, isVIP: true }, 1000) // true
-// isPayable({balance: 1000, isVIP: true }, 1000000) // true
+// Nếu như user không đủ tiền trả cho món hàng (balance ít hơn giá tiền) thì trả về false, còn không trả về true.
+//Tuy nhiên nếu như user đó là  (isVIP là true) thì dù không đủ tiền vẫn cho phép mua, trả về true
+let x6 = {
+  balance: 15000,
+  isVIP: true,
+};
+let y6 = 5000;
+function isPayable(x6, y6) {
+  if (x6.balance > y6 || x6.isVIP === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+isPayable({ balance: 1000000, isVIP: false }, 1000); // true
+isPayable({ balance: 1000, isVIP: false }, 1000000); // false
+isPayable({ balance: 1000000, isVIP: true }, 1000); // true
+isPayable({ balance: 1000, isVIP: true }, 1000000); // true
 
 // B7. Tạo function isPayable(x, y) nhận vào 2 tham số x, y
 // - tham sô x mô tả tình trạng balance (số dư tài khoản) của user, có dạng như sau:
@@ -105,11 +159,20 @@ const e10 = Number("12a") && !null; //NaN  (NaN && true)
 // isPayable({balance: 1000000, isVIP: true }, 1000) // true
 // isPayable({balance: 1000, isVIP: true }, 1000000) // true
 
-// B8. Tạo function daysCalc(x), nhận vào 1 tham số x mô tả 1 số là 1 tháng trong năm (1- 12). Trả về số ngày trong tháng đó dưới dạng 1 number. Tuy nhiên nếu như đối số truyền vào cho x không phải kiểu dữ liệu là number thì trả về chuỗi sau: "Wrong data type: argument is not a valid number"
-// VD:
-// daysCalc(12) // 31
-// daysCalc(11) // 30
-// daysCalc('abc') // "Wrong data type: argument is not a valid number"
+// B8. Tạo function daysCalc(x), nhận vào 1 tham số x mô tả 1 số là 1 tháng trong năm (1- 12).
+//Trả về số ngày trong tháng đó dưới dạng 1 number.
+//Tuy nhiên nếu như đối số truyền vào cho x không phải kiểu dữ liệu là number thì trả về chuỗi sau: "Wrong data type: argument is not a valid number"
+let x8 = 12;
+function daysCalc(x8) {
+  if (typeof x8 === "number") {
+    return 31;
+  } else {
+    return "Wrong data type: argument is not a valid number";
+  }
+}
+daysCalc(12); // 31
+daysCalc(11); // 30
+daysCalc("abc"); // "Wrong data type: argument is not a valid number"
 
 // B9.
 // - Tạo 1 mảng đại diện cho 1 nhóm 4 người, mỗi người được mô tả bằng một object như sau:
@@ -117,11 +180,20 @@ const e10 = Number("12a") && !null; //NaN  (NaN && true)
 // name: 1 string bất kỳ,
 // gender: là sring 'male' hoặc 'female'
 // }
-// VD: const testArr = [
-// {name: 'Harry', gender: 'male'},
-// {name: 'Hermione', gender: 'female'}
-// {name: 'Ron', gender: 'male'},
-// {name: 'Ginny', gender: 'female'}
-// ]
-// - Tạo 1 function girlFilter(x), nhận vào x là 1 mảng chính là mảng đã tạo bên trên. Lọc ra các thành viên là nữ (gender là 'female') trong mảng x, trả về 1 mảng khác chỉ chứa các thành viên nữ đó.
+// - Tạo 1 function girlFilter(x), nhận vào x là 1 mảng chính là mảng đã tạo bên trên.
+//Lọc ra các thành viên là nữ (gender là 'female') trong mảng x, trả về 1 mảng khác chỉ chứa các thành viên nữ đó.
 // VD: girlFilter(testArr) // [{name: 'Hermione', gender: 'female'}, {name: 'Ginny', gender: 'female'}]
+const testArr = [
+  { name: "Harry", gender: "male" },
+  { name: "Hermione", gender: "female" },
+  { name: "Ron", gender: "male" },
+  { name: "Ginny", gender: "female" },
+];
+function girlFilter(x9) {
+  if (testArr[1]["gender"] === "female") {
+    return testArr.testArr[1]["gender"] === "female";
+  } else {
+    return false;
+  }
+}
+girlFilter(testArr); // [{name: 'Hermione', gender: 'female'}, {name: 'Ginny', gender: 'female'}]
